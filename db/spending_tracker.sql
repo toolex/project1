168160059a3e90409b581cf67e73fe30e48e7e1f
Iain Tooley <iaintooley1@gmail.com>
2019-10-06 15:32:09 +0100

    fixed an error

--- db/spending_tracker.sql
+++ db/spending_tracker.sql
@@ -3,17 +3,17 @@ DROP TABLE tags;
 DROP TABLE merchants;
 
 CREATE TABLE tags(
-  id SERIAL8,
+  id SERIAL8 PRIMARY KEY,
   type VARCHAR(255)
 );
 
 CREATE TABLE merchants(
-  id SERIAL8,
+  id SERIAL8 PRIMARY KEY,
   name VARCHAR(255)
 );
 
 CREATE TABLE transactions(
-  id SERIAL8,
+  id SERIAL8 PRIMARY KEY,
   merchant_id INT8 REFERENCES merchants(id),
   tag_id INT8 REFERENCES tags(id),
   amount FLOAT8
