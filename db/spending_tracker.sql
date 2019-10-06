699a64ed0397e1f3b2c274ef5f00a674ac5f768c
Iain Tooley <iaintooley1@gmail.com>
2019-10-06 15:19:23 +0100

    added sql runner, started tables

---
+++ db/spending_tracker.sql
@@ -0,0 +1,18 @@
+DROP TABLE transactions;
+DROP TABLE tags;
+DROP TABLE merchants;
+
+CREATE TABLE tags(
+  id SERIAL8;
+
+)
+
+CREATE TABLE merchants(
+  id SERIAL8;
+
+)
+
+CREATE TABLE transactions(
+  id SERIAL8;
+
+)
