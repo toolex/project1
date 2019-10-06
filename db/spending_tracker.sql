7bfc10813753cf71520706f41764a3c727fe5a4e
Iain Tooley <iaintooley1@gmail.com>
2019-10-06 15:28:53 +0100

    finished tables used floats for amount

--- db/spending_tracker.sql
+++ db/spending_tracker.sql
@@ -3,16 +3,18 @@ DROP TABLE tags;
 DROP TABLE merchants;
 
 CREATE TABLE tags(
-  id SERIAL8;
-
-)
+  id SERIAL8,
+  type VARCHAR(255)
+);
 
 CREATE TABLE merchants(
-  id SERIAL8;
-
-)
+  id SERIAL8,
+  name VARCHAR(255)
+);
 
 CREATE TABLE transactions(
-  id SERIAL8;
-
-)
+  id SERIAL8,
+  merchant_id INT8 REFERENCES merchants(id),
+  tag_id INT8 REFERENCES tags(id),
+  amount FLOAT8
+);
