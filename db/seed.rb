7731674cc0dd53e6bfe246f6ed09f92192fa4f90
Iain Tooley <iaintooley1@gmail.com>
2019-10-07 09:48:02 +0100

    added seeds for transactions

--- db/seed.rb
+++ db/seed.rb
@@ -31,4 +31,32 @@ merchant3.save
 merchant4.save
 merchant5.save
 
-transaction1
+transaction1 = Transaction.new({
+  "merchant_id" => merchant1.id,
+  "tag_id" => tag1.id,
+  "amount" => 50
+  })
+
+transaction2 = Transaction.new({
+  "merchant_id" => merchant2.id,
+  "tag_id" => tag2.id,
+  "amount" => 10
+  })
+
+transaction3 = Transaction.new({
+  "merchant_id" => merchant3.id,
+  "tag_id" => tag3.id,
+  "amount" => 1.70
+  })
+
+transaction4 = Transaction.new({
+  "merchant_id" => merchant4.id,
+  "tag_id" => tag4.id,
+  "amount" => 100
+  })
+
+transaction5 = Transaction.new({
+  "merchant_id" => merchant5.id,
+  "tag_id" => tag5.id,
+  "amount" => 34.69
+  })
