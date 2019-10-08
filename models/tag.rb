b8b526528c0509078f0cb731017ee8746105b734
Iain Tooley <iaintooley1@gmail.com>
2019-10-08 13:40:54 +0100

    added some paths for tags

--- models/tag.rb
+++ models/tag.rb
@@ -24,6 +24,13 @@ class Tag
     @id = results.first()['id'].to_i
   end
 
+  def delete()
+    sql = "DELETE FROM tags
+    WHERE id = $1"
+    values = [@id]
+    SqlRunner.run(sql, values)
+  end
+
   def self.all()
   sql = "SELECT * FROM tags"
   results = SqlRunner.run(sql)
