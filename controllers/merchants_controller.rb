aec7d3318550b363015a372699290e1bcd5266ef
Iain Tooley <iaintooley1@gmail.com>
2019-10-08 09:12:42 +0100

    adding new merchant maker

--- controllers/merchants_controller.rb
+++ controllers/merchants_controller.rb
@@ -1,2 +1,14 @@
 require_relative( '../models/merchant.rb' )
 also_reload( '../models/*' )
+
+get '/transactions/new_merchant' do
+  @merchants = Merchant.all
+  @total_spent = Transaction.total
+  erb(:new_merchant)
+end
+
+post '/transactions' do
+  transaction = Merchant.new(params)
+  transaction.save
+  redirect to("/transactions")
+end
